490ea6caa84600d1a6643e28c962f800c46d4548
Jun-ya Norimatsu <do-not-send-to-this-address@jnory.com>
2017-02-10 18:13:00 +0900

    Create controller command.

--- scripts/create_options.py
+++ scripts/create_options.py
@@ -1,20 +1,20 @@
 import os.path
 
+def create():
+    basedir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
-basedir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+    options = [
+        "-p 4433:4433",
+        "-v {}/notebooks:/jupyter".format(basedir),
+        "-v {}/config/dot.jupyter:/root/.jupyter".format(basedir),
+        "-v {}/config/jupyter:/root/.local/share/jupyter".format(basedir),
+    ]
 
-options = [
-    "-p 4433:4433",
-    "-v {}/notebooks:/jupyter".format(basedir),
-    "-v {}/config/dot.jupyter:/root/.jupyter".format(basedir),
-    "-v {}/config/jupyter:/root/.local/share/jupyter".format(basedir),
-]
+    key = os.path.exists(os.path.join(basedir, "config/keys/jupyter.key"))
+    pem = os.path.exists(os.path.join(basedir, "config/keys/jupyter.pem"))
 
-key = os.path.exists(os.path.join(basedir, "config/keys/jupyter.key"))
-pem = os.path.exists(os.path.join(basedir, "config/keys/jupyter.pem"))
+    if key and pem:
+        options.append("-v {}/config/keys:/etc/cert".format(basedir))
 
-if key and pem:
-    options.append("-v {}/config/keys:/etc/cert".format(basedir))
-
-print(" ".join(options))
+    return " ".join(options).split(" ")
 
